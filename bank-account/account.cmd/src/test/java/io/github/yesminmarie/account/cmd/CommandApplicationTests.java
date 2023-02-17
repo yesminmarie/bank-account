57f2e49d5e02eaa1ac009607465921ca14a00c8b
Yesmin Lahoud <ymslahoud@gmail.com>
2023-02-17 09:18:14 -0300

    bank-account

---
+++ bank-account/account.cmd/src/test/java/io/github/yesminmarie/account/cmd/CommandApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.yesminmarie.account.cmd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ bank-account/account.query/src/test/java/io/github/yesminmarie/account/query/QueryApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.yesminmarie.account.query;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
